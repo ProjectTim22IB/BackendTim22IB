bd68407e3dcd865c00db8075990dd9297475ac19
Filip Vuksan <96701802+VuksanFilip@users.noreply.github.com>
2023-06-17 01:19:13 +0200

    commit

--- src/main/java/com/example/service/TwilioService.java
+++ src/main/java/com/example/service/TwilioService.java
@@ -6,6 +6,9 @@ import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import org.springframework.stereotype.Service;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @Service
 public class TwilioService implements ITwilioService {
 
@@ -14,7 +17,7 @@ public class TwilioService implements ITwilioService {
                     + "<p>Aktivaciju naloga možete obaviti klikom na sledeći link:</p>";
 
     public static final String ACCOUNT_SID = "AC019f343b932c807b4384d3c978abb1f2";
-    public static final String AUTH_TOKEN = "8a68a393cd36c7764fdfba1f060479ad";
+    public static final String AUTH_TOKEN = "814df2934749e8b56050ea6039afc071";
     public static final String FROM_PHONE_NUMBER = "whatsapp:+14155238886";
     public static final String TO_PHONE_NUMBER = "whatsapp:+381665423903";
 
@@ -30,8 +33,8 @@ public class TwilioService implements ITwilioService {
 
     @Override
     public void sendActivationSMS(String toPhoneNumber, UserActivation activation) {
-
-        String activationLink = "http://localhost:8084/api/user/activate/" + activation.getId();
+//
+//        String activationLink = "http://" + getLocalIPv4() + "/api/user/activate/" + activation.getId();
 //        String body = this.activationMessage +
 //                "<a href='" + activationLink +"'>" + activationLink;
 
@@ -39,7 +42,17 @@ public class TwilioService implements ITwilioService {
         Message message = Message.creator(
                 new com.twilio.type.PhoneNumber(TO_PHONE_NUMBER),
                 new com.twilio.type.PhoneNumber(FROM_PHONE_NUMBER),
-                "http://").create();
+                getLocalIPv4()).create();
         System.out.println(message.getSid());
+    }
+
+    public static String getLocalIPv4() {
+        try {
+            InetAddress localhost = InetAddress.getLocalHost();
+            return localhost.getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
